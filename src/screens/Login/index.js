1f64289c08e0f185c789d9e36bb5c2dda935d901
Jonathan Byron <jonathan.byron2908@gmail.com>
2018-07-29 10:28:08 +0200

    Project configs

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,6 +1,6 @@
 import { Button, Content, Form, Text, View } from 'native-base';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Platform } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Actions } from 'react-native-router-flux';
@@ -11,43 +11,47 @@ import InputReduxForm from '../../components/form/InputReduxForm';
 import { loginSaga } from '../../sagas/login';
 import { getUserSaga } from '../../sagas/user';
 
-const Login = ({ submit, onGetUserGraphQLClick }) => (
-  <KeyboardAwareScrollView
-    enableOnAndroid
-    enableAutomaticScroll
-    keyboardOpeningTime={0}
-    extraHeight={Platform.select({ android: 200 })}
-    extraScrollHeight={Platform.select({ android: 10 })}
-  >
-    <Content>
+class Login extends PureComponent {
+  render() {
+    const { submit, onGetUserGraphQLClick } = this.props;
+    return (
+      <KeyboardAwareScrollView
+        enableOnAndroid
+        enableAutomaticScroll
+        keyboardOpeningTime={0}
+        extraHeight={Platform.select({ android: 200 })}
+        extraScrollHeight={Platform.select({ android: 10 })}
+      >
+        <Content>
 
-      <Form>
-        <Field name="email" component={InputReduxForm} label="Email" />
-        <Field name="password" component={InputReduxForm} label="Password" secureTextEntry />
-      </Form>
+          <Form>
+            <Field name="email" component={InputReduxForm} label="Email" />
+            <Field name="password" component={InputReduxForm} label="Password" secureTextEntry />
+          </Form>
 
-      <View style={{ marginTop: 10 }} flexDirection="row" justifyContent="center" >
+          <View style={{ marginTop: 10 }} flexDirection="row" justifyContent="center" >
 
-        <Button style={{ marginRight: 25 }} onPress={() => submit()}>
-          <Text>Login</Text>
-        </Button>
+            <Button style={{ marginRight: 25 }} onPress={() => submit()}>
+              <Text>Login</Text>
+            </Button>
 
-        <Button onPress={() => Actions.SignUp()}>
-          <Text>Sign up</Text>
-        </Button>
+            <Button onPress={() => Actions.SignUp()}>
+              <Text>Sign up</Text>
+            </Button>
 
-      </View>
+          </View>
 
-      <Button onPress={() => onGetUserGraphQLClick()}>
-        <Text>
+          <Button onPress={() => onGetUserGraphQLClick()}>
+            <Text>
           Get user
-        </Text>
-      </Button>
+            </Text>
+          </Button>
 
-    </Content>
-  </KeyboardAwareScrollView>
-);
-
+        </Content>
+      </KeyboardAwareScrollView>
+    );
+  }
+}
 
 Login.propTypes = {
   submit: PropTypes.func.isRequired,
