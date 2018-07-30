7ab099429ddba340fe050ad8c138189dfa8e2099
Jonathan Byron Correa Lindoso <jonathan.byron2908@gmail.com>
2018-07-30 15:47:34 +0200

    Remove ReduxStorage and change LocalStorage folder

--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -12,9 +12,30 @@ class LocalStorage {
     return result;
   }
 
+  async loadObject(key, defaultValue = null) {
+    let result = defaultValue;
+    try {
+      const value = await AsyncStorage.getItem(key);
+      result = (!value) ? defaultValue : JSON.parse(value);
+    } catch (error) {
+      result = defaultValue;
+    }
+    return result;
+  }
+
   async save(key, data) {
     try {
       await AsyncStorage.setItem(key, data);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async saveObject(key, data) {
+    try {
+      const dataStr = JSON.stringify(data);
+      await AsyncStorage.setItem(key, dataStr);
       return true;
     } catch (error) {
       return false;
---
+++ src/localStorage/localStorageKeys.js
@@ -0,0 +1,8 @@
+
+export const TOKEN_SK = 'token';
+
+const localStorageKeys = {
+  TOKEN_SK,
+};
+
+export default localStorageKeys;
