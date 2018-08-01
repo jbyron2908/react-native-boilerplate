655d7fa0019d70c07deb300c8ec343d71eb805cb
Jonathan Byron Correa Lindoso <jonathan.byron2908@gmail.com>
2018-08-01 18:04:59 +0200

    UpdateStore saga

---
+++ src/sagas/updateStore.js
@@ -0,0 +1,56 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { updateCategoriesAction } from '../reducers/categories';
+import database from '../rxdb/database/database';
+import { updateAccountsAction } from '../reducers/accounts';
+import { updateTransactionsAction } from '../reducers/transactions';
+
+// Actions
+const UPDATE_STORE = 'saga/auth/update_store';
+
+// Saga
+function* saga() {
+  console.log('updateStoreSaga');
+  const categories = yield call(getCategories);
+  yield put(updateCategoriesAction(categories));
+
+  const accounts = yield call(getAccounts);
+  yield put(updateAccountsAction(accounts));
+
+  const transactions = yield call(getTransactions);
+  yield put(updateTransactionsAction(transactions));
+}
+
+async function getCategories() {
+  console.log('getCategories');
+  const db = await database.getInstance();
+  const categories = await db.categories.find().exec();
+  console.log(categories);
+  return categories;
+}
+
+async function getAccounts() {
+  console.log('getAccounts');
+  const db = await database.getInstance();
+  const accounts = await db.accounts.find().exec();
+  console.log(accounts);
+  return accounts;
+}
+
+async function getTransactions() {
+  console.log('getTransactions');
+  const db = await database.getInstance();
+  const transactions = await db.transactions.find().exec();
+  console.log(transactions);
+  return transactions;
+}
+
+export default function* () {
+  yield takeLatest(UPDATE_STORE, saga);
+}
+
+// Action Creators
+export function updateStoreSaga() {
+  return {
+    type: UPDATE_STORE,
+  };
+}
