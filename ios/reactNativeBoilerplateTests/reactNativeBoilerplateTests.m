30d16d3177c6171c0ef82f161419a77446d55451
Jonathan Byron <jonathan.byron2908@gmail.com>
2018-08-18 09:17:24 +0200

    Change configurations

--- ios/reactNativeBoilerplateTests/reactNativeBoilerplateTests.m
+++ ios/reactNativeBoilerplateTests/reactNativeBoilerplateTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface myfinanceTests : XCTestCase
+@interface reactNativeBoilerplateTests : XCTestCase
 
 @end
 
-@implementation myfinanceTests
+@implementation reactNativeBoilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
