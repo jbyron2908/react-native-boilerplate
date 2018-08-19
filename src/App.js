1285fa8383c473ba032bd1472546b0e9a449e379
Jonathan Byron <jonathan.byron2908@gmail.com>
2018-08-19 22:30:19 +0200

    Init RxDB

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Container, Root } from 'native-base';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
 import debugConfig from './config/debugConfig';
 import { store } from './redux/config/reduxStore';
 import NavigatorService from './views/navigation/NavigatorService';
 import RootNavigator from './views/navigation/RootNavigator';
+import database from './rxdb/database/database';
 
 debugConfig();
 
@@ -12,12 +13,21 @@ const setNavigator = (navigatorRef) => {
   NavigatorService.setNavigator(navigatorRef);
 };
 
-export default () => (
-  <Provider store={store}>
-    <Root>
-      <Container>
-        <RootNavigator ref={navigatorRef => setNavigator(navigatorRef)} />
-      </Container>
-    </Root>
-  </Provider>
-);
+
+export default class App extends PureComponent {
+  async componentWillMount() {
+    await database.init();
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Root>
+          <Container>
+            <RootNavigator ref={navigatorRef => setNavigator(navigatorRef)} />
+          </Container>
+        </Root>
+      </Provider>);
+  }
+}
+
